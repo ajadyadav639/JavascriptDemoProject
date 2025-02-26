35d8a307a20e3a61ff9758c331a16f628b9d4729
ajadyadav639 <yadavajad639@gmail.com>
2025-02-26 08:32:48 +0000

    my codes

---
+++ basics/datatype.js
@@ -0,0 +1,9 @@
+let name = "Ajad Yadav"
+let age = 22
+let isloggedin = false
+let Email;
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof false)
+console.log(typeof null)
+console.log(typeof Email)
---
+++ basics/test.js
@@ -0,0 +1,5 @@
+const accountId=1234;
+var password="9936";
+let name="Ajad Yadav"
+console.log("Hy I Am Ajad Yadav");
+console.table([accountId,password,name ]);
