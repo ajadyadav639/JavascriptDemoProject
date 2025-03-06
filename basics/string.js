f110fc89ec6b38ea238da7e4533f64a78b757bb1
ajadyadav639 <yadavajad639@gmail.com>
2025-03-06 03:44:56 +0000

    save

---
+++ basics/string.js
@@ -0,0 +1,42 @@
+// let name = new String('Hitesh')
+// console.log(name);
+// console.log(name[0]);
+// console.log(name.__proto__);
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.charAt(5));
+// console.log(name.indexOf('h'));
+
+// const gameName = new String('Ajad-Yadav')
+// console.log(gameName.substring(0,5));
+// console.log(gameName.length);
+
+// const gName = new String('hiteshhc')
+// const ssname = gName.substring(0, 4)
+// console.log(ssname);
+
+// const anotherName = gName.slice(-8, 6);
+// console.log(anotherName);
+
+// const myName = "         Ajad Yadav "
+// console.log(myName);
+// console.log(myName.trim());
+
+const url = "https://ajadyadav.com/Ajad%20Yadav"
+console.log(url.replace('%20','-'));
+console.log(url.includes('Ajay'));
+
+
+
+const sName = "Ajad-Yadav";
+console.log(sName);
+
+console.log(sName.split('-'))
+
+
+
+
+
+
+
+
