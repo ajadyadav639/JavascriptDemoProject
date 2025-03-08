ee4dd5bf86425a837d054b8ac7aac6602e411f7a
ajadyadav639 <yadavajad639@gmail.com>
2025-03-08 03:57:51 +0000

    save

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+// const myArr = [0,1,2,3,4,5]
+// console.log(myArr);
+// console.log(myArr[3]);
+
+// const myHeroes = ['Ajad Yadav','Abhishek Yadav','Ajay Yadav','Anuj Yadav','Ansh Yadav','Adarsh Yadav','Abyansh']
+// console.log(myHeroes[0]);
+
+// let newArr = new Array(10,20,30,40,50,60,70,80,90,100)
+// console.log(newArr);
+// newArr.push(110,120,130,140)
+// newArr.pop()
+// console.log(newArr);
+
+// let sArr = [10,20,30,40]
+// console.log(sArr);
+// sArr.unshift(5,6,7,8,9)
+// sArr.shift()
+// console.log(sArr);
+// console.log(sArr.includes(40));
+// console.log(sArr.indexOf(30));
+// console.log(sArr.indexOf(50));
+
+// let newAr = sArr.join()
+// console.log(sArr);
+// console.log(newAr);
+// console.log(typeof newAr);
+
+
+const myArr = [10,20,30,40,50,60]
+console.log("A ",myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+
+console.log("B ",myArr);
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log(myArr);
+
+
+
+
+
+
+
