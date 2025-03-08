5f83a3fe9aecfc826fe234037a975cf2f00bd5ac
ajadyadav639 <yadavajad639@gmail.com>
2025-03-08 02:01:18 +0000

    savemycode

---
+++ 01_basics/dates.js
@@ -0,0 +1,23 @@
+// let date = new Date();
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
+
+
+// let myCreatedDate = new Date(2025,0,1);
+// console.log(myCreatedDate.toDateString());
+
+
+// let nowDate =  Date.now();
+// console.log(nowDate);
+// console.log(date.getTime());
+// console.log(date.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth());
+
+newDate.toLocaleString('default', {weekday: "long"})
+
