e03c5f5b4ed519132d09de1c1b2ab8448561cd3a
ajadyadav639 <yadavajad639@gmail.com>
2025-03-11 16:25:36 +0000

    save

---
+++ 03_basics/04_life.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions 
+
+(function chai() {
+    console.log("DB Connected");
+})();
+
+(function aurCode(name) {
+    console.log(`DB Two Connected ${name}`);
+    
+    
+})("Ajad");
