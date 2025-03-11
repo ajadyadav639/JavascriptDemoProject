28f2fa60673103609b4c9d24d46627e6a9b34a12
ajadyadav639 <yadavajad639@gmail.com>
2025-03-11 05:09:09 +0000

    save

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,13 +1,36 @@
-function syMyName() {
-    console.log("A");
-    console.log("j");
-    console.log("a");
-    console.log("d");
-    console.log(" ");
-    console.log("Y");
-    console.log("a");
-    console.log("d");
-    console.log("a");
-    console.log("v");
+// function syMyName() {
+//     console.log("A");
+//     console.log("j");
+//     console.log("a");
+//     console.log("d");
+//     console.log(" ");
+//     console.log("Y");
+//     console.log("a");
+//     console.log("d");
+//     console.log("a");
+//     console.log("v");
+// }
+// syMyName()
+
+function addTwoNumbers(number1,number2) {
+    // console.log(number1+number2);
+    let result = number1 + number2
+    return  result
+    
 }
-syMyName()
+const result = addTwoNumbers(10,20)
+// console.log("Result",result);
+
+// addTwoNumbers(10,20)
+// addTwoNumbers(10,"20")
+// addTwoNumbers(10,null)
+// addTwoNumbers(10)
+
+function loginUserMessage(username) {
+    if (username === undefined) {
+        console.log("Please Enter The Username");
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMessage());
