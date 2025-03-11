1d2d1e884921c73f1631a9e511669dc06e3ac5bb
ajadyadav639 <yadavajad639@gmail.com>
2025-03-11 06:39:55 +0000

    save

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -12,13 +12,15 @@
 // }
 // syMyName()
 
-function addTwoNumbers(number1,number2) {
-    // console.log(number1+number2);
-    let result = number1 + number2
-    return  result
+
+
+// function addTwoNumbers(number1,number2) {
+//     // console.log(number1+number2);
+//     let result = number1 + number2
+//     return  result
     
-}
-const result = addTwoNumbers(10,20)
+// }
+// const result = addTwoNumbers(10,20)
 // console.log("Result",result);
 
 // addTwoNumbers(10,20)
@@ -26,11 +28,37 @@ const result = addTwoNumbers(10,20)
 // addTwoNumbers(10,null)
 // addTwoNumbers(10)
 
-function loginUserMessage(username) {
-    if (username === undefined) {
-        console.log("Please Enter The Username");
-        return
-    }
-    return `${username} just logged in`
+// function loginUserMessage(username) {
+//     if (username === undefined) {
+//         console.log("Please Enter The Username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(loginUserMessage());
+
+
+
+// const user = {
+//     name: "Ajad Yadav",
+//     price : 200
+// }
+
+// function handleObject(anyObject) {
+//     console.log(`My Name Is ${anyObject.name} and My Fruits Price Is ${anyObject.price}`);
+    
+// }
+// // handleObject(user)
+// handleObject({
+//     name : "Ankit Yadav",
+//     price : 400
+// })
+
+
+const myNewArray = [100,200,300,400,500,600]
+function getArray(anyArray) {
+    return anyArray[3]
 }
-console.log(loginUserMessage());
+// console.log(getArray(myNewArray));
+console.log(getArray([10,20,30,40,50,60]));
+
