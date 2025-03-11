a4aa68b88fcd82370964d5925f83fffb228fef3d
ajadyadav639 <yadavajad639@gmail.com>
2025-03-11 04:08:04 +0000

    save

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,13 @@
+function syMyName() {
+    console.log("A");
+    console.log("j");
+    console.log("a");
+    console.log("d");
+    console.log(" ");
+    console.log("Y");
+    console.log("a");
+    console.log("d");
+    console.log("a");
+    console.log("v");
+}
+syMyName()
