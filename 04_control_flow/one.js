0d8ea5291595a0fb0f0af7ae7d88d8f7315c63ea
ajadyadav639 <yadavajad639@gmail.com>
2025-03-13 06:50:27 +0000

    s

---
+++ 04_control_flow/one.js
@@ -0,0 +1,32 @@
+// const balance =2000
+
+// if (balance<500) {
+//     console.log("Balance less than 500");
+    
+// } else if (balance<1000) {
+//     console.log("Balance less than 1000");
+// } else if (balance<1500) {
+//     console.log("Balance less than 1500");
+// }else if (balance<2000) {
+//     console.log("Balance less than 2000");
+// }else {
+//     console.log("Our Balance is Enough For Shopping");
+// }
+
+
+userLoggedIn = true
+debitCard = true
+
+if (userLoggedIn && debitCard && 2==="2" ) {
+    console.log("Now You can Buy the Book");
+    
+}
+
+
+loggedInFromGoogle = true
+loggedInFromREmail = false
+
+if (loggedInFromGoogle || loggedInFromREmail || 2==3) {
+    console.log("You Have Valid User For this Interface");
+    
+}
