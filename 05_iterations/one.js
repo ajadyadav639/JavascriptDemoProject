f884a9caaa64b2b1b909c6998596eb932b9d7ea4
ajadyadav639 <yadavajad639@gmail.com>
2025-03-20 12:16:15 +0000

    s

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,6 +1,32 @@
 
 for (let i = 1; i <= 10; i++) {
     const element = i;
-    console.log(i);
+    //console.log(i);
+    
+}
+
+
+for (let i = 1; i <=10; i++) {
+    // console.log(`Outer loop is : ${i}`);
+    
+    for (let j = 1; j <=10; j++) {
+    //   console.log(`inner loop is : ${j} and outer loop is ${i}`);
+        
+        
+    }
+    
+}
+
+
+
+
+for (let i = 1; i <=10; i++) {
+    console.log(`Outer loop is : ${i}`);
+    
+    for (let j = 1; j <=10; j++) {
+      console.log(i + ' * ' + j + ' = ' + i*j);
+        
+        
+    }
     
 }
