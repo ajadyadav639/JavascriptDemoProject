17d2060552e405a17f4e856b3521db774c564844
ajadyadav639 <yadavajad639@gmail.com>
2025-08-11 07:10:57 +0000

    save

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -30,5 +30,24 @@ promiseTwo.then(function (user){
 
 const promiseFour = new Promise(function (resolve, reject) {
     setTimeout(function(){
-
+        let error = false;
+        if (!error) {
+            resolve({username: "Ajay Yadav", Email: "yadavajay639@gmail.com"});
+        } else {
+            reject('Error: Something went wrong');
+        }
     }, 1000)
+})
+promiseFour.then( (user) => {
+  console.log(user);
+  return user.username
+  
+}).then((username) => {
+  console.log(username);
+}).catch((error) => {
+  console.error(error);
+}).finally(() => {
+  console.log('Promise execution completed');
+});
+
+
