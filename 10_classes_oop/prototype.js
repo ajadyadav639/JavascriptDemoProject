d0d184ee117a0f82ecf9b19c0b41e28727139d07
ajadyadav639 <yadavajad639@gmail.com>
2025-08-12 04:24:20 +0000

    s

--- 10_classes_oop/prototype.js
+++ 10_classes_oop/prototype.js
@@ -1,7 +1,34 @@
-let myHeros = [ 'Thor', 'spiderman']
+let myHeros = ["thor", "spiderman"]
+
 
 let heroPower = {
-    Thor : 'hammer',
-    spiderman : 'sling',
-    
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.hitesh = function(){
+    console.log(`hitesh is present in all objects`);
+}
+
+Array.prototype.heyHitesh = function(){
+    console.log(`Hitesh says hello`);
+}
+
+ heroPower.hitesh()
+ myHeros.hitesh()
+myHeros.heyHitesh()
+// heroPower.heyHitesh()
+
+// inheritance
+
+const Teacher = {
+    makeVideo : true,
+}
+
+const TeacherSupport = {
+    isAvailable: false,
 }
